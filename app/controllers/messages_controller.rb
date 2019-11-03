72017693dcba1c220f730bd2278a702984e29bb5
Nicolas Corbalan <outreef@hotmail.com>
2019-11-04 00:59:38 +0100

    adding class MessagesController

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,9 @@
-def create
-    Message.create(message_params)
-end
+class MessagesController < ApplicationController
+    def create
+        Message.create(message_params)
+    end
 
-def message_params
-    params.require(:message).permit(:author, :content)
+    def message_params
+        params.require(:message).permit(:content)
+    end
 end
-
