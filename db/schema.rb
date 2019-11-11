43b3bc795b3c38ad8d4c42048efb25c879130559
Nicolas Corbalan <outreef@hotmail.com>
2019-11-11 20:59:16 +0100

    rails db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_25_222454) do
+ActiveRecord::Schema.define(version: 2019_11_11_193823) do
 
   create_table "messages", force: :cascade do |t|
     t.string "content"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
   end
 
 end
